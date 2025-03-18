778871884f78e9ca01ec6b3b1e4f8c800227aa03
shaan Yadav <shaanyworkspace@gmail.com>
2025-03-18 16:03:28 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -10,41 +10,7 @@ import { AuthConstent } from './context/AuthProvider.jsx'
 function App() {
 	let [user,setUser] = useState(null);
 	const [loggedInUserData,setLoggedInUserData] = useState(null);
-	const Authdata = useContext(AuthConstent);
-
-	useEffect(()=>{
-		let loggedInUser = localStorage.getItem("loggedInUser");
-		if(loggedInUser){
-			setUser(loggedInUser.role)
-		}
-	},[Authdata]);
-
-	useEffect(()=>{
-		setLocalStorage();
-	})
-	
-	
-	const identifyUser = (email,password)=>{
-		if (!Authdata) {
-			alert("Invalid user");
-		} else {
-			const admin = Authdata.admin?.find((e) => e.email === email && e.password === password);
-			const employee = Authdata.employees?.find((e) => e.email === email && e.password === password);
-		
-			if (admin) {
-				setUser("admin");
-				setLoggedInUserData(admin);
-				localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
-			} else if (employee) {
-				setUser("employee");
-				setLoggedInUserData(employee);
-				localStorage.setItem("loggedInUser", JSON.stringify({ role: "employee" }));
-			} else {
-				alert("Invalid user credentials");
-			}
-		}		
-	}
-	
+	const Authdata = useContext(AuthConstent);	
 	return (
     <>
 		{!user ? (<Login identifyUser={identifyUser} />) : user === "admin" ? (<AdminDashboard changeUser={setUser} data={loggedInUserData} />) : <EmployeeDashboard  changeUser={setUser}  data={loggedInUserData}/>}
